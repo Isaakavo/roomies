706ada7e1ea3ad905eb318547c67ac905babf3a6
isaakalejandro.haas.contractor <isaakalejandro.haas.contractor@bbva.com>
2021-10-26 18:29:10 -0500

    Change of .db file name

--- server/main.py
+++ server/main.py
@@ -4,12 +4,11 @@ from models.sharedDb import db
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///temp/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/roomies.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+
 db.init_app(app)
-
-
 
 if __name__ == '__main__':
   app.run(debug = True)
