4d02e63aa559cf5ab7d765e451dc07570d30eb7e
isaakalejandro.haas.contractor <isaakalejandro.haas.contractor@bbva.com>
2022-01-06 20:40:42 -0600

    feat: creation of tokenSchema and TokenDataschema

--- server/schemas/schemas.py
+++ server/schemas/schemas.py
@@ -11,12 +11,20 @@ class UserSchema(ma.Schema):
 class TasksSchema(ma.Schema):
   id = fields.Integer(dump_only = True)
   task = fields.String()
-  username = fields.String()
+  # username = fields.String()
   description = fields.String()
   created = fields.DateTime('%d-%m-%Y %H:%M:%S')
   ended = fields.DateTime('%d-%m-%Y %H:%M:%S')
+  access_token = fields.String()
 
 class LoginSchema(ma.Schema):
   username = fields.String()
   password = fields.String()
 
+class TokenSchema(ma.Schema):
+  access_token = fields.String()
+  # token_type = fields.String()
+
+class TokenDataSchema(ma.Schema):
+  userId = fields.Integer()
+  exp = fields.Integer()
